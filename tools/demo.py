6847b8760654b17e5e9cce609eff6a3020afa075
cbhust8025 <bocheng8025@gmail.com>
2018-01-04 16:26:19 +0800

    1、规范格式

--- tools/demo.py
+++ tools/demo.py
@@ -21,7 +21,10 @@ from utils.timer import Timer
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
-import caffe, os, sys, cv2
+import caffe
+import os
+import sys
+import cv2
 import argparse
 
 CLASSES = ('__background__',
@@ -34,7 +37,7 @@ CLASSES = ('__background__',
 NETS = {'vgg16': ('VGG16',
                   'VGG16_faster_rcnn_final.caffemodel'),
         'zf': ('ZF',
-                  'ZF_faster_rcnn_final.caffemodel')}
+               'ZF_faster_rcnn_final.caffemodel')}
 
 
 def vis_detections(im, class_name, dets, thresh=0.5):
@@ -56,7 +59,7 @@ def vis_detections(im, class_name, dets, thresh=0.5):
                           bbox[2] - bbox[0],
                           bbox[3] - bbox[1], fill=False,
                           edgecolor='red', linewidth=3.5)
-            )
+        )
         ax.text(bbox[0], bbox[1] - 2,
                 '{:s} {:.3f}'.format(class_name, score),
                 bbox=dict(facecolor='blue', alpha=0.5),
@@ -65,10 +68,11 @@ def vis_detections(im, class_name, dets, thresh=0.5):
     ax.set_title(('{} detections with '
                   'p({} | box) >= {:.1f}').format(class_name, class_name,
                                                   thresh),
-                  fontsize=14)
+                 fontsize=14)
     plt.axis('off')
     plt.tight_layout()
     plt.draw()
+
 
 def demo(net, image_name):
     print "Run the demo()"
@@ -90,14 +94,15 @@ def demo(net, image_name):
     CONF_THRESH = 0.8
     NMS_THRESH = 0.3
     for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1 # because we skipped background
-        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_ind += 1  # because we skipped background
+        cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
         cls_scores = scores[:, cls_ind]
         dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
         vis_detections(im, cls, dets, thresh=CONF_THRESH)
+
 
 def parse_args():
     """Parse input arguments."""
@@ -114,6 +119,7 @@ def parse_args():
     args = parser.parse_args()
     print "args:", args
     return args
+
 
 if __name__ == '__main__':
     print "Run the __main__()"
@@ -143,7 +149,7 @@ if __name__ == '__main__':
     # Warmup on a dummy image
     im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
     for i in xrange(2):
-        _, _= im_detect(net, im)
+        _, _ = im_detect(net, im)
 
     im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
                 '001763.jpg', '004545.jpg']
